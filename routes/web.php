2e300237a4adc42b6e366f12518dba0afe4c4398
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-09 21:02:22 +0100

    tes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
 });
 
-Route::get('/about', function() {
+Route::get('/about', function () {
     return view('here.about');
 });
